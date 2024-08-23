28a42eabca55e4cf5512f144dbeb9e1ed6936dfb
DylanJinx <2636353989@qq.com>
2024-08-23 16:24:49 +0800

    modified: ServiceNFT_A.sol - mint_A()

--- src/ServiceNFT_A.sol
+++ src/ServiceNFT_A.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interface/IServiceNFT_A.sol";
 
-contract ServiceNFT_A is ERC721URIStorage, Ownable {
+contract ServiceNFT_A is IServiceNFT_A, ERC721URIStorage, Ownable(msg.sender) {
     uint256 private _currentTokenId = 0;
 
     constructor(string memory _name, string memory _symbol) 
         ERC721(_name, _symbol) 
-        Ownable(msg.sender)
     {}
 
-    function mintTo(address _recipient, string memory _uri) public onlyOwner returns (uint256) {
+    function mint_A(address _recipient, string memory _uri) public returns (uint256) {
         uint256 newTokenId = _currentTokenId++;
         _mint(_recipient, newTokenId);
         _setTokenURI(newTokenId, _uri);
