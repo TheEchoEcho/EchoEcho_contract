a19086492a5de97ac4603ebd719d827bdd517b40
DylanJinx <2636353989@qq.com>
2024-08-27 13:42:10 +0800

    print test results

---
+++ TEXT/EchoEcho_Test.sh
@@ -0,0 +1,75 @@
+No files changed, compilation skipped
+
+Ran 17 tests for test/EchoEcho_Test.sol:EchoEcho_Test
+[PASS] test_buy() (gas: 563609)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_buyWithSign() (gas: 571748)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_buy_consumer2() (gas: 597986)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_cancelList() (gas: 321404)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_cancelListWithSign() (gas: 332779)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_cancelOrder() (gas: 654596)
+Logs:
+  Minted NFT with tokenId: 0
+  consumer_1 balance: 50
+  provider_1 balance: 49
+
+[PASS] test_cancelOrder_ListSign() (gas: 662680)
+Logs:
+  Minted NFT with tokenId: 0
+  consumer_1 balance: 50
+  provider_1 balance: 49
+
+[PASS] test_fail1_cancelOrder() (gas: 568163)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail2_cancelOrder() (gas: 575023)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_NFT_A_safeTransferFrom() (gas: 117009)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_NFT_A_safeTransferFrom_data() (gas: 117255)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_NFT_A_transferFrom() (gas: 116911)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_buy_consumer2() (gas: 574744)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_cancelList() (gas: 296666)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_fail_list() (gas: 18613)
+[PASS] test_fail_serviceWithdraw() (gas: 572191)
+Logs:
+  Minted NFT with tokenId: 0
+
+[PASS] test_list() (gas: 290410)
+Logs:
+  Minted NFT with tokenId: 0
+
+Suite result: ok. 17 passed; 0 failed; 0 skipped; finished in 3.61ms (10.96ms CPU time)
+
+Ran 1 test suite in 671.91ms (3.61ms CPU time): 17 tests passed, 0 failed, 0 skipped (17 total tests)
